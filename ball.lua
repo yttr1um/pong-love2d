be0d65c14ba4064cda6962791b5748137f90e28a
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 17:06:54 +0400

    ball bounces off of player and top and bottom walls

--- ball.lua
+++ ball.lua
@@ -1,5 +1,7 @@
 local Ball = {}
 
+local Player = require("player")
+
 function Ball:load()
     self.x = love.graphics.getWidth() / 2
     self.y = love.graphics.getHeight() / 2
@@ -12,11 +14,32 @@ end
 
 function Ball:update(dt)
     self:move(dt)
+    self:collide()
 end
 
 function Ball:move(dt)
     self.x = self.x + self.xVel * dt
     self.y = self.y + self.yVel * dt
+end
+
+function Ball:collide()
+    if checkCollision(self, Player) then
+        self.xVel = self.speed
+
+        local middleBall = self.y + self.height / 2
+        local middlePlayer = Player.y + Player.height / 2
+        local collisionPosition = middleBall - middlePlayer
+        self.yVel =  collisionPosition * 5
+    end
+
+    if self.y < 0 then 
+        self.y = 0
+        self.yVel = -self.yVel
+
+    elseif self.y + self.height > love.graphics.getHeight() then
+        self.y = love.graphics.getHeight() - self.height
+        self.yVel = -self.yVel
+    end
 end
 
 function Ball:draw()
