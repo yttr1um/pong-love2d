f9803ec317227e0b69c9698bf6eb18ccb3293c91
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 17:39:27 +0400

    opp ai follows ball

--- ai.lua
+++ ai.lua
@@ -1,15 +1,33 @@
 local AI = {}
 
+local Ball = require("ball")
+
 function AI:load()
     self.width = 20
     self.height = 100
     self.x = love.graphics.getWidth() - self.width - 50
     self.y = love.graphics.getHeight() / 2
-    self.speed = 200
+    self.speed = 500
+    self.yVel = 0
 end
 
 function AI:update(dt)
+    self:move(dt)
+    self:acquireTarget()
+end
 
+function AI:move(dt)
+    self.y = self.y + self.yVel * dt
+end
+
+function AI:acquireTarget()
+    if Ball.y + Ball.height < self.y then
+        self.yVel = -self.speed
+    elseif Ball.y > self.y + self.height then
+        self.yVel = self.speed
+    else
+        self.yVel = 0
+    end
 end
 
 function AI:draw()
