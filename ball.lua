ef430105943ef419500b655b79c29d922d8273f3
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 15:11:40 +0400

    Inital ball setup

---
+++ ball.lua
@@ -0,0 +1,15 @@
+local Ball = {}
+
+function Ball:load()
+
+end
+
+function Ball:update(dt)
+
+end
+
+function Ball:draw()
+
+end
+
+return Ball
--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
 local Player = require("player")
+local Ball = require("ball")
 
 function love.load()
     Player:load()
+    Ball:load()
 end
 
 function love.update(dt)
     Player:update(dt)
+    Ball:update(dt)
 end
 
 function love.draw()
     Player:draw()
+    Ball:draw()
 end
