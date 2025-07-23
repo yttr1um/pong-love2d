448ade6c803f393ff5ac14a7107dafcac46a9f74
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 14:38:48 +0400

    added player in main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,13 @@
 local Player = require("player")
 
 function love.load()
-
+    Player:load()
 end
 
 function love.update(dt)
-
+    Player:update(dt)
 end
 
 function love.draw()
-
+    Player:draw()
 end
