94b618adea2671456c3c57dcc0324b09d6e81104
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 15:32:37 +0400

    draw ball and add movement

--- ball.lua
+++ ball.lua
@@ -1,15 +1,26 @@
 local Ball = {}
 
 function Ball:load()
-
+    self.x = love.graphics.getWidth() / 2
+    self.y = love.graphics.getHeight() / 2
+    self.width = 10
+    self.height = 10
+    self.speed = 200
+    self.xVel = -self.speed
+    self.yVel = 0
 end
 
 function Ball:update(dt)
+    self:move(dt)
+end
 
+function Ball:move(dt)
+    self.x = self.x + self.xVel * dt
+    self.y = self.y + self.yVel * dt
 end
 
 function Ball:draw()
-
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 return Ball
