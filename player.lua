dafb0c324c148cd4e0467fbebf48ea7522d13615
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-23 14:51:10 +0400

    Added player on screen and player movement

--- player.lua
+++ player.lua
@@ -1,15 +1,27 @@
 local Player = {}
 
 function Player:load()
-
+    self.x = 50
+    self.y = love.graphics.getHeight() / 2
+    self.width = 20
+    self.height = 100
+    self.speed = 500
 end
 
 function Player:update(dt)
+    self:move(dt)
+end
 
+function Player:move(dt)
+    if love.keyboard.isDown("w", "up") then
+        self.y = self.y - self.speed * dt
+    elseif love.keyboard.isDown("s", "down") then
+        self.y = self.y + self.speed * dt
+    end
 end
 
 function Player:draw()
-
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 return Player
